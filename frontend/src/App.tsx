29d2179ecbd19ca82718539b6a48337315109ffb
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2025-02-02 16:49:02 +0400

    upd. 020225 1648

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import TasksList from './components/TasksList';
 import CalendarEvents from './components/CalendarEvents';
