75da304141eaa05eb59e8b4abecf5b0b726d5102
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2025-02-04 01:00:55 +0400

    fix

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 import pytz
 import logging
 from flask_sqlalchemy import SQLAlchemy
+from uuid import uuid4
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
