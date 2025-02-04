3880ea82512460d2ec9aefa74b86a88222780b7e
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2025-02-05 00:15:07 +0400

    time upd.

--- frontend/src/components/TasksList.tsx
+++ frontend/src/components/TasksList.tsx
@@ -95,6 +95,7 @@ const TasksList: React.FC = () => {
     try {
       const taskData: any = {
         title: newTaskInput,
+        due: dueDate || undefined,
         list_id: selectedTasklistId, // ID списка задач
       };
 
@@ -240,9 +241,6 @@ const TasksList: React.FC = () => {
                             new Date(task.due).getMonth() !== new Date().getMonth() ||
                             new Date(task.due).getDate() !== new Date().getDate() ? (
                             <span>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar" viewBox="0 0 16 16">
-                                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-                              </svg>
                               {new Date(task.due).toLocaleDateString([], {
                                 year: 'numeric',
                                 month: '2-digit',
@@ -257,10 +255,6 @@ const TasksList: React.FC = () => {
                           </svg>
                           {new Date(task.due).getHours() !== 0 || new Date(task.due).getMinutes() !== 0 ? (
                             <span>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clock" viewBox="0 0 16 16">
-                                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-                                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
-                              </svg>
                               {new Date(task.due).toLocaleTimeString([], {
                                 hour: '2-digit',
                                 minute: '2-digit',
