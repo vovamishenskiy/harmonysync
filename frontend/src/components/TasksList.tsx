36b86e78a9d4dd97a488625c3e60f498798196ac
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2025-02-06 19:23:15 +0400

    date debug 2

--- frontend/src/components/TasksList.tsx
+++ frontend/src/components/TasksList.tsx
@@ -236,13 +236,9 @@ const TasksList: React.FC = () => {
                         <svg data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" stroke-linecap="round" stroke-linejoin="round"></path>
                         </svg>
-                        {new Date(task.due).getFullYear() !== new Date().getFullYear() ||
-                          new Date(task.due).getMonth() !== new Date().getMonth() ||
-                          new Date(task.due).getDate() !== new Date().getDate() ? (
-                          <span>
-                            {task.due_date}
-                          </span>
-                        ) : null}
+                        <span>
+                          {task.due_date}
+                        </span>
                       </div>
                     )}
                     {task.due_time && (
@@ -250,11 +246,9 @@ const TasksList: React.FC = () => {
                         <svg data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" stroke-linecap="round" stroke-linejoin="round"></path>
                         </svg>
-                        {new Date(task.due_time).getHours() !== 0 || new Date(task.due).getMinutes() !== 0 ? (
-                          <span>
-                            {task.due_time}
-                          </span>
-                        ) : null}
+                        <span>
+                          {task.due_time}
+                        </span>
                       </div>
                     )}
                     <button className="task-delete-button" onClick={() => handleTaskDelete(task.id)}
