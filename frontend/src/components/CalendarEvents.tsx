3231a58ff7fe907c82900d093bd9d87b30fae351
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2025-10-30 15:31:27 +0400

    minor visual upd.

--- frontend/src/components/CalendarEvents.tsx
+++ frontend/src/components/CalendarEvents.tsx
@@ -30,9 +30,17 @@ const CalendarEvents: React.FC = () => {
   return (
     <div className="calendar-section">
       <div className="calendar-header">
-        <button onClick={handlePrevMonth} className="calendar-nav">◀</button>
+        <button onClick={handlePrevMonth} className="calendar-nav">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+          </svg>
+        </button>
         <h2 className="calendar-month">{monthName} {currentYear}</h2>
-        <button onClick={handleNextMonth} className="calendar-nav">▶</button>
+        <button onClick={handleNextMonth} className="calendar-nav">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+          </svg>
+        </button>
       </div>
 
       <div className="calendar-weekdays">
