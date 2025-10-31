f9d8b4387eeb1f6ec442c200979131f26fb35fff
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2025-10-31 13:23:02 +0400

    front upd.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -122,7 +122,7 @@ function App() {
         <div className="header-actions">
           {user && (
             <div className="user-info">
-              <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="presentation"><path d="M7 15h10a2 2 0 0 1 2 2v4h2v-4a4 4 0 0 0-4-4H7a4 4 0 0 0-4 4v4h2v-4a2 2 0 0 1 2-2Z"></path><path d="M12 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm0 2a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
+              <svg className={`theme-icon ${theme === 'dark' ? 'dark' : 'light'}`} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="presentation"><path d="M7 15h10a2 2 0 0 1 2 2v4h2v-4a4 4 0 0 0-4-4H7a4 4 0 0 0-4 4v4h2v-4a2 2 0 0 1 2-2Z"></path><path d="M12 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm0 2a5 5 0 1 0 0-10 5 5 0 0 0 0 10Z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
               <span>{user.email}</span>
             </div>
           )}
